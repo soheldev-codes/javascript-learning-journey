f435916b251fb67234ce9c3487deafc41c098a24
soheldev.codes <soheldev.codes@gmail.com>
2026-01-22 22:45:44 +0600

    Learn Javascript Run Basic

---
+++ Intro_Javascript/Javascript_Run_Basics/Browser_Run/script.js
@@ -0,0 +1,3 @@
+console.log("JavaScript is running in the browser");
+
+alert("Hello from Browser!");
---
+++ Intro_Javascript/Javascript_Run_Basics/Node_Js_Run/App.js
@@ -0,0 +1,10 @@
+// Show Browser Console and terminal
+
+
+console.log("JavaScript is running using Node.js");
+
+let num1 = 10;
+
+let num2 = 20;
+
+console.log("Sum:", num1 + num2);
